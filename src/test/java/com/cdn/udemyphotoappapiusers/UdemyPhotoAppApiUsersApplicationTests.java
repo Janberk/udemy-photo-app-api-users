cfa83df4c399a6bc0b9ab8e4bc3cdda7699ea490
cdn <cdn@dasburo.com>
2022-09-11 01:11:51 +0200

    chore: initial setup

---
+++ src/test/java/com/cdn/udemyphotoappapiusers/UdemyPhotoAppApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdn.udemyphotoappapiusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyPhotoAppApiUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
